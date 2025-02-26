a73bf9400e4c4209cb13013140dc8b1e23ed4dc3
Farodigme <daniil.tkachenko01@mail.ru>
2025-02-26 22:55:10 +0300

    Initial

--- Collectors-Corner.Server/obj/Debug/net8.0/Collectors-Corner.Server.AssemblyInfo.cs
+++ Collectors-Corner.Server/obj/Debug/net8.0/Collectors-Corner.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collectors-Corner.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d84ff4abab75b987178fd20ab3c2de7174800e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collectors-Corner.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collectors-Corner.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
