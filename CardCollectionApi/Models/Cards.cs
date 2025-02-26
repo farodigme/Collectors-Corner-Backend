efb250122922d3f3c6bef79ac7ba4a45c415a848
Farodigme <daniil.tkachenko01@mail.ru>
2025-02-26 22:53:16 +0300

    New architecture

--- CardCollectionApi/Models/Cards.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CardCollectionApi.Models
-{
-	public class Cards
-	{
-		public int Id { get; set; }
-		public string? Title { get; set; }
-		public string? Description { get; set; }
-	}
-}
--- CardCollectionApi/Models/Collections.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CardCollectionApi.Models
-{
-	public class Collections
-	{
-		public int Id { get; set; }
-		public string? Title {  get; set; }
-		public string? Description { get; set; }
-	}
-}
