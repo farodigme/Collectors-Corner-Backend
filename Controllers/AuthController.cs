3bd508a110a874a7f62a23281b8abb719ff3de38
Farodigme <tudra14@mail.ru>
2025-03-02 20:03:23 +0300

    add jwt create method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,30 +1,45 @@
+using Collectors_Corner_Backend.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Collectors_Corner_Backend.Controllers
 {
 	[ApiController]
-	[Route("[controller]")]
+	[Route("api/[controller]")]
 	public class AuthController : ControllerBase
 	{
 		private readonly ILogger<AuthController> _logger;
+		private readonly JwtSettings _jwtSettings;
 
-		public AuthController(ILogger<AuthController> logger)
+		public AuthController(ILogger<AuthController> logger, IOptions<JwtSettings> jwtSettings)
 		{
 			_logger = logger;
+			_jwtSettings = jwtSettings.Value;
 		}
 
-		[HttpGet("HealthCheck")]
-		public IActionResult HealthCheck()
+		public string GenerateJwtToken(string username)
 		{
-			return Ok("Work");
+			var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+			var jwt = new JwtSecurityToken(
+				issuer: _jwtSettings.Issuer,
+				audience: _jwtSettings.Audience,
+				claims: claims,
+				expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+				signingCredentials: new SigningCredentials(_jwtSettings.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+			return new JwtSecurityTokenHandler().WriteToken(jwt);
 		}
 
-		[Authorize]
-		[HttpGet("Test")]
-		public IActionResult Test()
+		[HttpGet("login/{username}")]
+		public IActionResult Login(string username)
 		{
-			return Ok("Test");
+			if (string.IsNullOrEmpty(username)) return BadRequest("Empty username");
+
+			return Ok(GenerateJwtToken(username));
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Collectors_Corner_Backend.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Text;
+
 namespace Collectors_Corner_Backend
 {
 	public class Program
@@ -5,18 +11,32 @@ namespace Collectors_Corner_Backend
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			var configuration = builder.Configuration;
+
+			builder.Services.Configure<JwtSettings>(configuration.GetRequiredSection("JwtSettings"));
 
 			builder.Services.AddControllers();
-			builder.Services.AddAuthentication("Bearer").AddJwtBearer();
 			builder.Services.AddAuthorization();
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+					{
+						ValidateIssuer = true,
+						ValidIssuer = configuration["JwtSettings:Issuer"],
+						ValidateAudience = true,
+						ValidAudience = configuration["JwtSecrets:Audience"],
+						ValidateLifetime = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"])),
+						ValidateIssuerSigningKey = true
+					};
+				});
 
 			var app = builder.Build();
 
 			app.UseHttpsRedirection();
-
 			app.UseAuthentication();
 			app.UseAuthorization();
-
 			app.MapControllers();
 
 			app.Run();
