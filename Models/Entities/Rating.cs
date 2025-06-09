b330d726533709b7c5e139395ff64977c71a06fe
Farodigme <tudra14@mail.ru>
2025-06-09 20:40:31 +0300

    feat: rating entity

---
+++ Models/Entities/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace Collectors_Corner_Backend.Models.Entities
+{
+	public class Rating
+	{
+		public int Id { get; set; }
+		public int CollectionId { get; set; }
+		public Collection? Collection { get; set; }
+		public int UserId { get; set; }
+		public User? User { get; set; }
+		public decimal StarCount { get; set; }
+	}
+}
