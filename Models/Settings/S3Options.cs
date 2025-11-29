8c406b99d578a8c47a3ccb2fa4b256dee51c33c4
Farodigme <tudra14@mail.ru>
2025-11-29 15:22:56 +0300

    feat: add S3

---
+++ Models/Settings/S3Options.cs
@@ -0,0 +1,10 @@
+﻿namespace Collectors_Corner_Backend.Models.Settings
+{
+	public class S3Options
+	{
+		public string AWS_BUCKET_NAME { get; set; } = null!;
+		public string AWS_ACCESS_KEY {  get; set; } = null!;
+		public string AWS_SECRET_KEY { get;set; } = null!;
+		public string AWS_SERVICE_URL { get; set; } = null!;
+	}
+}
---
+++ Services/S3Service.cs
@@ -0,0 +1,15 @@
+﻿using Collectors_Corner_Backend.Models.Settings;
+
+namespace Collectors_Corner_Backend.Services
+{
+	public class S3Service
+	{
+		private S3Options _options;
+		private IAmazonS3 _s3;
+		public S3Service(S3Options s3Options, IAmazonS3 s3)
+		{
+			_options = s3Options;
+			_s3 = s3;
+		}
+	}
+}
